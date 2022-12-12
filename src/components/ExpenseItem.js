4f26510f70c1bdbd88bbc118e70e27ff11411dfd
PavankumarMula <pavanpappy7@gmail.com>
2022-12-12 11:51:19 +0530

    added Location

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,17 @@
-import './ExpenseItem.css'
+import "./ExpenseItem.css";
 function ExpenseItem() {
+  const date = new Date().getDate();
+  const ExpenseItem = "car Insurance";
+  const expenseAmount = "$294.25";
+  const location = "India";
   return (
     <div className="expense-item">
-      <div>Dec 12</div>
+      <div><h2 color='white'>{date}</h2></div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.15
-        </div>
+        <h2>{ExpenseItem}</h2>
+        <div className="expense-item__price">{expenseAmount}</div>
       </div>
+      <div className="expense-item__location"><h2>{location}</h2></div>
     </div>
   );
 }
