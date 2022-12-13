309bfa02c93fbc76211b9cb7284974476ad35e63
PavankumarMula <pavanpappy7@gmail.com>
2022-12-13 22:24:01 +0530

    useState

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,22 +1,22 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 import ExpenseDate from './ExpenseDate';
 import Card from './Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-  const remove=()=>{
-    console.log("deleted");
+  const [amount,setMoney]=useState(props.amount);
+  const changeCurrency=()=>{
+    setMoney("$100");
   }
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
+        <div className='expense-item__price'>{amount}</div>
       </div>
-      <button onClick={console}>Click Me</button>
-      <button onClick={remove}>Delete</button>
+      <button onClick={changeCurrency}>Click Me</button>
     </Card>
   );
 }
