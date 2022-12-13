16876a7a17c560a28d81dd8132231101e7c0ce3d
PavankumarMula <pavanpappy7@gmail.com>
2022-12-13 16:36:08 +0530

    added de;ete button

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -5,6 +5,9 @@ import Card from './Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+  const remove=()=>{
+    console.log("deleted");
+  }
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
@@ -12,6 +15,8 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
+      <button onClick={console}>Click Me</button>
+      <button onClick={remove}>Delete</button>
     </Card>
   );
 }
