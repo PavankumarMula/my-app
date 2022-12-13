faa43ad460b3518072b3e37aa3f33b3765d88ff4
PavankumarMula <pavanpappy7@gmail.com>
2022-12-13 16:28:18 +0530

    Standard App

--- src/App.js
+++ src/App.js
@@ -1,41 +1,41 @@
-import './App.css';
-import './components/ExpenseItem'
-import ExpenseItem from './components/ExpenseItem';
-const App=()=> {
-  const data=[{id:1,Date:new Date(2022,12,14),title:"Car Insurance",Amount:'$100',location:"hyderabad"},
-  {id:2,Date:new Date(2022,1,27),title:"Movies",Amount:'$200',location:"bangalore"},
-  {id:3,Date:new Date(2022,3,8),title:"Pizza",Amount:'$300',location:"mumbai"},
-  {id:4,Date:new Date(2022,11,15),title:"Cricket",Amount:'$400',location:"chennai"}]
+import React from 'react';
+import Expenses from './components/Expenses';
+
+const App = () => {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
   return (
     <div>
-      <h2> Lets get Started</h2>
-      <ExpenseItem
-       date={data[0].Date}
-       description={data[0].title}
-       Amount={data[0].Amount}
-       location={data[0].location}       
-       ></ExpenseItem>
-      <ExpenseItem
-      date={data[1].Date}
-      description={data[1].title}
-      Amount={data[1].Amount}
-      location={data[1].location}       
-      ></ExpenseItem>
-      <ExpenseItem
-      date={data[2].Date}
-      description={data[2].title}
-      Amount={data[2].Amount}
-      location={data[2].location}       
-      ></ExpenseItem>
-      <ExpenseItem
-      date={data[3].Date}
-      description={data[3].title}
-      Amount={data[3].Amount}
-      location={data[3].location}       
-      ></ExpenseItem>
-    </div> 
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses} />
+    </div>
   );
 }
-
-
 export default App;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,19 @@
-import "./ExpenseItem.css";
-import './ExpenseDate';
-import ExpenseDetails from "./ExpenseDetails";
-import ExpenseDate from "./ExpenseDate";
-const ExpenseItem=(props)=> {
+import React from 'react';
+
+import ExpenseDate from './ExpenseDate';
+import Card from './Card';
+import './ExpenseItem.css';
+
+const ExpenseItem = (props) => {
   return (
-    <div className="expense-item">
-      <ExpenseDate date={props.date}></ExpenseDate>
-      <ExpenseDetails Amount={props.Amount}
-      description={props.description}
-      ></ExpenseDetails>
-      <div className="expense-item__location"><h2>{props.location}</h2></div>
-    </div>
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+    </Card>
   );
 }
+
 export default ExpenseItem;
