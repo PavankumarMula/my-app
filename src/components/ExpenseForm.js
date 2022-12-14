79e15a53df1df612b2f46077642021536f2f5f71
PavankumarMula <pavanpappy7@gmail.com>
2022-12-14 12:25:48 +0530

    Console INputs

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,5 +1,12 @@
 import "./ExpenseForm.css";
 const ExpenseForm = () => {
+    const showConsole =(event)=>{
+        event.preventDefault();
+        const inputs=document.getElementsByTagName('input');
+        for (let x of inputs){
+            console.log(x.value);
+        }
+    }
   return (
     <form>
       <div className="new-expense__controls">
@@ -9,15 +16,15 @@ const ExpenseForm = () => {
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="$1" step="$2" />
+          <input type="number" min="$1" step="$2"  />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2023-12-31" />
+          <input type="date" min="2019-01-01" max="2023-12-31"  />
         </div>
       </div>
       <div className="new-expense__actions">
-        <button type="submit">Add Expense</button>
+        <button type="submit" onClick={showConsole}>Add Expense</button>
       </div>
     </form>
   );
